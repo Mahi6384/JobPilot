27644c0bc3deec790a9024196677e780f44ae626
KhushiChauhan8 <kc6478576@gmail.com>
2025-07-19 18:34:53 +0530

    connecting server with mongodb atlas

--- server/index.js
+++ server/index.js
@@ -1,8 +1,28 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
 app.use(express.json());
-app.get("/hello", (req, res) => res.send("jkbfsjfh"));
+
+// MongoDB connection
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("MongoDB connected successfully!");
+  })
+  .catch((error) => {
+    console.error("MongoDB connection failed:", error.message);
+  });
+
+
+app.get("/hello", (req, res) => res.send("Hello from Khushi’s server!"));
+
+
 app.listen(PORT, () => {
   console.log(`Server is running at: http://localhost:${PORT}`);
 });

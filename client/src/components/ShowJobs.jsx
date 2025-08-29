267492e7ba7869691a40420659cf49638cf4fb20
Mahi Jain <mahij.2912@gmail.com>
2025-08-30 00:24:43 +0530

    display all job cards with real-time data from backend

---
+++ client/src/components/ShowJobs.jsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+function ShowJobs() {
+  const [jobs, setJobs] = useState([]);
+  async function getJob() {
+    try {
+      const response = await axios.get("http://localhost:5000/api/jobs");
+      setJobs(response.data.data);
+    } catch (error) {
+      console.log("Error", error);
+    }
+  }
+  useEffect(() => {
+    getJob();
+  }, []);
+
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-12 font-montserrat font-medium">
+      {jobs.map((jobCard, index) => (
+        <div key={index} className="flex flex-col items-center">
+          <div className="bg-gray-950 w-full max-w-sm shadow-[0_0_10px_2px_theme(colors.blue.900)] text-blue-600 p-8 rounded-t-md">
+            <p className="truncate">Name : {jobCard.title}</p>
+            <p className="truncate">Company : {jobCard.companyName}</p>
+            <p className="truncate">Location : {jobCard.location}</p>
+            <p className="truncate">Experience : {jobCard.experience}</p>
+          </div>
+          <button className="w-full max-w-sm shadow-[0_3px_15px_2px_theme(colors.blue.900)] bg-blue-900 text-gray-950 text-lg py-3 rounded-b-md">
+            Apply
+          </button>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default ShowJobs;
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,46 +1,14 @@
-import React from "react";
-import { useState } from "react";
+import ShowJobs from "../components/ShowJobs";
+
 function Dashboard() {
-  // const [name, setName] = useState("");
-  // const [company, setCompany] = useState("");
-  // const [location, setLocation] = useState("");
-  // const [experience, setExperience] = useState("");
-
-  const dummyData = [
-    {
-      name: "Front-end Developer",
-      company: "Google",
-      location: "Gurgaon",
-      experience: "Fresher",
-    },
-    {
-      name: "Back-end Developer",
-      company: "Microsoft",
-      location: "Delhi",
-      experience: "1year",
-    },
-    {
-      name: "Full Stack Developer",
-      company: "Amazon",
-      location: "Bangalore",
-      experience: "2 year",
-    },
-  ];
-
-  return dummyData.map((jobCard) => (
-    <div className=" flex mt-8">
-      <div>
-        <p>Review Applications and submit</p>
-        <div className="bg-slate-600 flex flex-col p-5 text-white">
-          <p>JOB Title: {jobCard.name} </p>
-          <p>Company Name :{jobCard.company}</p>
-          <p>Location : {jobCard.location} </p>
-          <p>Experience {jobCard.experience} </p>
-          <p>Skills match - 98%</p>
-        </div>
-      </div>
+  return (
+    <div className="min-h-screen bg-gray-950 overflow-y-auto scrollbar-none p-4">
+      <h1 className="text-white text-3xl font-montserrat font-semibold pl-10 pb-5">
+        New Generated Jobs
+      </h1>
+      <ShowJobs />
     </div>
-  ));
+  );
 }
 
 export default Dashboard;

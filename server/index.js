d15d1513a24aa1075ba463436ba2f8652a742450
Mahi Jain <mahij.2912@gmail.com>
2025-08-29 16:10:09 +0530

    fixing cors error

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 const app = express();
 app.use(express.json());
+app.use(cors());
 const PORT = process.env.PORT || 5000;
 mongoose
   .connect(process.env.MONGODB_URI)

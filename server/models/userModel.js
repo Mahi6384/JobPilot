4c2c7066b531fdeed4baab16df718328cf38aa31
Mahi Jain <mahij.2912@gmail.com>
2025-08-29 13:47:06 +0530

    CREATED job card Schema, Model

---
+++ server/models/userModel.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const useModel = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: [true, "Username is required"],
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", useModel);
+module.exports = User;
